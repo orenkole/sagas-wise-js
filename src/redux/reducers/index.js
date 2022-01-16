0defa0231fff2d9750a53c4b33d33857391241d4
badger <badger@badger.local>
2022-01-16 18:26:22 +0200

    lection 2. initial setup

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,28 +1,8 @@
-const initial = {
-	people: [],
-	planets: [],
-}
+const initial = { }
 
 export default function reducer(state = initial, action) {
 	switch(action.type) {
-		case "SET_PEOPLE": {
-			return {
-				...state,
-				people: [
-					...state.people,
-					...action.payload
-				]
-			}
-		}
-		case "SET_PLANETS": {
-			return {
-				...state,
-				planets: [
-					...state.planets,
-					...action.payload
-				]
-			}
-		}
+
 		default:
 			return state;
 	}
