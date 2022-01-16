d4c33713da41fc99d878db71cc636ebf07d4b6bc
badger <badger@badger.local>
2022-01-16 18:48:33 +0200

    rootSaga setup. p1

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,3 +1,30 @@
+import {all, call, spawn} from "redux-saga/effects";
+
+export function* saga1() {
+	console.log('Saga 1')
+}
+export function* saga2() {
+	console.log('Saga 2')
+}
+export function* saga3() {
+	console.log('Saga 3')
+}
+
 export default function* rootSaga() {
-	console.log('hello from saga')
+	const sagas = [saga1, saga2, saga2];
+
+	const retrySagas =  sagas.map(saga => {
+		return spawn(function* () {
+			while(true) {
+				try {
+					yield call(saga);
+					break;
+				} catch(e) {
+					console.log(e)
+				}
+			}
+		})
+	})
+	
+	yield all(retrySagas);
 }
