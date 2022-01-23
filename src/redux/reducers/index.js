7aafa1730955450d38f5f080bdf900b3096d5f1b
badger <badger@badger.local>
2022-01-23 13:47:58 +0200

    clean old saga

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,26 +3,9 @@ import {connectRouter} from "connected-react-router";
 import {createBrowserHistory} from 'history';
 import peopleReducer from "./people";
 
-const initial = {
-	blog: {}
-}
-
 export const history = createBrowserHistory()
 
-export function appReducer(state = initial, action) {
-	switch(action.type) {
-		case 'BLOG_LOADED': 
-			return {
-				...state,
-				blog: action.payload
-			}
-		default:
-			return state;
-	}
-}
-
 const rootReducer = combineReducers({
-	app: appReducer,
 	router: connectRouter(history),
 	people: peopleReducer,
 })
