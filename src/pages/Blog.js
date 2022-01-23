cc7c1826eb2866b0599ce6d65ddd135fa9964620
badger <badger@badger.local>
2022-01-23 08:45:59 +0200

    handle use click. take + fork

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,9 +1,19 @@
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 
 export default function Blog () {
 	const blogData = useSelector(store => store.app.blog);
 	console.log('blog data', blogData);
+	const dispatch = useDispatch()
 	return (
-		<div>Blog</div>
+		<div>
+			Blog
+			<button
+				onClick={() => {
+					dispatch({type: 'LOAD_SOME_DATA'})
+				}}
+			>
+				Load some data
+			</button>	
+		</div>
 	)
 }
--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,11 +1,27 @@
-import {all, call, spawn} from "redux-saga/effects";
+import {all, call, fork, spawn, take} from "redux-saga/effects";
 import loadBasicData from "./initialSagas";
 import pageLoaderSaga from "./pageLoader";
+
+export function* fetchPlanets() {
+	console.log('LOAD_SOME_DATA starts')
+
+	const response = yield call(fetch, 'https://swapi.dev/api/planets')
+	const data = yield call([response, response.json])
+	console.log('LOAD_SOME_DATA completed', data)
+}
+
+export function* loadOnAction() {
+	while(true) {
+		yield take ('LOAD_SOME_DATA');
+		yield fork(fetchPlanets);
+	}
+}
 
 export default function* rootSaga() {
 	const sagas = [
 		loadBasicData,
-		pageLoaderSaga
+		pageLoaderSaga,
+		loadOnAction
 	];
 
 	const retrySagas =  sagas.map(saga => {
